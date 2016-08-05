fb1eeb46c7ec38b8f927f48ef2f72607e048aa36
joaobolsson <joaovictorbolsson@gmail.com>
2016-08-05 02:21:04 -0300

    Corrigindo funcionalidade para exportar itens para o banco. Esta funcionando perfeitamente (verificar codigo)

--- class/Util.class.php
+++ class/Util.class.php
@@ -54,40 +54,34 @@ class Util extends Conexao {
 	 *	@return array
 	 */
 	public function preparaImportacao($tmp_name) {
-		// Abre o Arquvio no Modo r (para leitura)
-		$arquivo = fopen($tmp_name, 'r');
-		$values = "";
 		$insert = "INSERT INTO itens VALUES";
+		$values = "";
+		$row = 1;
 		$array_sql = array();
-		$index = $cont = 0;
-		$array_sql[$index] = $insert;
-		while (!feof($arquivo)) {
-			$linha = fgets($arquivo, 1024);
-			$dados = explode('	', $linha);
-			for ($i = 0; $i < count($dados); $i++) {
-				$dados[$i] = $this->mysqli->real_escape_string($dados[$i]);
+		$i = $pos = 0;
+		if (($handle = fopen($tmp_name, "r")) !== FALSE) {
+			while (($data = fgetcsv($handle, 1000, "	")) !== FALSE) {
+				if ($data[0] != "ID_ITEM_PROCESSO") {
+					$row++;
+					for ($c = 0; $c < count($data); $c++) {
+						$data[$c] = $this->mysqli->real_escape_string($data[$c]);
+					}
+					$values .= "\n(NULL, {$data[0]}, {$data[1]}, '{$data[2]}', '{$data[3]}', '{$data[4]}', '{$data[5]}', '{$data[6]}', '{$data[7]}', '{$data[8]}', '{$data[9]}', '{$data[10]}', '{$data[11]}', '{$data[12]}', '{$data[13]}', '{$data[14]}', '{$data[15]}', '{$data[16]}', '{$data[17]}', '{$data[18]}', '{$data[19]}', '{$data[20]}', '{$data[21]}', {$data[22]}, '{$data[23]}', {$data[24]}, '{$data[25]}', {$data[26]}, '{$data[27]}', '{$data[28]}', '{$data[29]}', 0), ";
+					if ($row == 70) {
+						$pos = strrpos($values, ", ");
+						$values[$pos] = ";";
+						$array_sql[$i] = $insert . $values;
+						$values = "";
+						$i++;
+						$row = 1;
+					}
+				}
 			}
-			if ($dados[0] != "ID_ITEM_PROCESSO") {
-				$values .= "(NULL, {$dados[0]}, {$dados[1]}, '{$dados[2]}', '{$dados[3]}', '{$dados[4]}', '{$dados[5]}', '{$dados[6]}', '{$dados[7]}', '{$dados[8]}', '{$dados[9]}', '{$dados[10]}', '{$dados[11]}', '{$dados[12]}', '{$dados[13]}', '{$dados[14]}', '{$dados[15]}', '{$dados[16]}', '{$dados[17]}', '{$dados[18]}', '{$dados[19]}', '{$dados[20]}', '{$dados[21]}', {$dados[22]}, '{$dados[23]}', {$dados[24]}, '{$dados[25]}', {$dados[26]}, '{$dados[27]}', '{$dados[28]}', '{$dados[29]}', 0), ";
-				$cont++;
-			}
-			if ($cont == 200) {
-				$array_sql[$index] = $insert . $values . ";";
-				$cont = 0;
-				$values = "";
-				$pos = strrpos($array_sql[$index], ", ");
-				$array_sql[$index][$pos] = "";
-				$index++;
-			}
+			fclose($handle);
+			$pos = strrpos($values, ", ");
+			$values[$pos] = ";";
+			$array_sql[$i] = $insert . $values;
 		}
-		$array_sql[$index] = $insert . $values . ";";
-		fclose($arquivo);
-		$max = count($array_sql) - 1;
-		$pos = strrpos($array_sql[$max], ", ");
-		if ($pos) {
-			$array_sql[$max][$pos] = "";
-		}
-		echo $array_sql[0];
 		return $array_sql;
 	}
 	/**
