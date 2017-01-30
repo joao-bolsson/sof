4e65637239eb72a0cf7a0400b4174235dc37eba0
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-01-30 14:22:36 -0200

    testar compatibilidade da datatables no ie9

--- lte/apoio.php
+++ lte/apoio.php
@@ -44,6 +44,13 @@ $obj_Busca = new BuscaLTE();
         <link rel="stylesheet" href="dist/css/snackbar.min.css">
 
         <link rel="icon" href="../favicon.ico">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
     <body class="hold-transition skin-blue layout-top-nav" onload="iniSolicitacoes();">
--- lte/index.php
+++ lte/index.php
@@ -47,6 +47,13 @@ $count = $obj_Busca->getCountSolic();
         <link rel="stylesheet" href="dist/css/snackbar.min.css">
 
         <link rel="icon" href="../favicon.ico">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body class="hold-transition skin-blue sidebar-mini" onload="iniAdminSolicitacoes();">
         <div class="wrapper">
