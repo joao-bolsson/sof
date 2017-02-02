7081973f22ef188a8eaabead2f1eb79baa4a9a3a
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-02-02 12:23:11 -0200

    iniciando com grunt, adicionando licence

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        uglify: {
+            options: {
+                compress: false,
+                mangle: false
+            },
+
+            my_target: {
+                files: {
+                    'ini.min.js': ['ini.js'],
+                    'iniLTE.min.js': ['iniLTE.js']
+                }
+            }
+        } // uglify
+
+    });
+
+
+    // Plugins do Grunt
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Tarefas que serão executadas
+    grunt.registerTask('default', ['uglify']);
+
+};
