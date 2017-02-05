f2f8b10e9a1b7301e8b8e17b00aacad875f1d9a4
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-02-05 18:09:33 -0200

    retira tarefa para deploy com grunt (fica para v2.1.5)

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,33 +14,15 @@ module.exports = function (grunt) {
                     'iniLTE.min.js': ['iniLTE.js']
                 }
             }
-        }, // uglify
-
-        ftpush: {
-            build: {
-                auth: {
-                    host: 'ftp.sofhusm.net.br',
-                    port: 21,
-                    authKey: 'key'
-                },
-                src: './',
-                dest: './web/',
-                exclusions: ['./ini.js', './iniLTE.js', './node_modules', './*.sql', '.git', '.gitignore', '.ftppass', 'package.json', 'Gruntfile.js', './*.md'],
-                keep: ['./uploads/*'],
-                simple: true
-            }
-        }
+        } // uglify
     });
 
 
     // Plugins do Grunt
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-ftpush');
 
     // Tarefas que serão executadas
     grunt.registerTask('default', ['uglify']);
 
-    // Tarefa para deploy
-    grunt.registerTask('f', ['ftpush']);
 
 };
