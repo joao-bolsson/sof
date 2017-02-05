37739fbcc0de8b68b6e8606d4d21612e6afbe5bc
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-02-05 17:49:57 -0200

    mudando plugin de deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,8 +15,8 @@ module.exports = function (grunt) {
                 }
             }
         }, // uglify
-        
-       'ftp-deploy': {
+
+        ftpush: {
             build: {
                 auth: {
                     host: 'ftp.sofhusm.net.br',
@@ -25,21 +25,22 @@ module.exports = function (grunt) {
                 },
                 src: './',
                 dest: './web/',
-                exclusions: ['./ini.js', './iniLTE.js', './node_modules', './*.sql', '.git', '.gitignore', '.ftppass', 'package.json', 'Gruntfile.js', './*.md']
+                exclusions: ['./ini.js', './iniLTE.js', './node_modules', './*.sql', '.git', '.gitignore', '.ftppass', 'package.json', 'Gruntfile.js', './*.md'],
+                keep: ['./uploads/*'],
+                simple: true
             }
         }
-
     });
 
 
     // Plugins do Grunt
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-ftp-deploy');
+    grunt.loadNpmTasks('grunt-ftpush');
 
     // Tarefas que serão executadas
     grunt.registerTask('default', ['uglify']);
-    
+
     // Tarefa para deploy
-    grunt.registerTask('f', ['ftp-deploy']);
+    grunt.registerTask('f', ['ftpush']);
 
 };
