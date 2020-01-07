33b65810124022c7f6770bd82b2f22d64b999886
João Bolsson <joaovictorbolsson@gmail.com>
2020-01-07 00:08:08 -0300

    foreign key check

--- auto_increment.php
+++ auto_increment.php
@@ -4,9 +4,11 @@ include_once 'class/Query.class.php';
 
 $tables = ["pedido_contrato", "pedido_empenho", "pedido_fonte", "pedido_grupo", "pedido_id_fonte", "pedido_log_status", "pedido_plano"];
 
+Query::getInstance()->exe("SET foreign_key_checks = 0;");
 $query = Query::getInstance()->exe("SELECT id FROM pedido");
 
 $i = 1;
+
 while ($obj = $query->fetch_object()) {
     foreach ($tables as $table) {
         Query::getInstance()->exe("UPDATE " . $table . " SET id_pedido = " . $i . " WHERE id_pedido = " . $obj->id);
@@ -15,5 +17,7 @@ while ($obj = $query->fetch_object()) {
 }
 
 Query::getInstance()->exe("ALTER TABLE pedido AUTO_INCREMENT = " . $i);
+
+Query::getInstance()->exe("SET foreign_key_checks = 1;");
 
 echo "Terminou!";
