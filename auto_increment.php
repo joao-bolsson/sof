6069e47c706decc438dc87ec2defd4385d8b492a
João Bolsson <joaovictorbolsson@gmail.com>
2020-01-07 00:12:51 -0300

    another fix

--- auto_increment.php
+++ auto_increment.php
@@ -4,7 +4,7 @@ include_once 'class/Query.class.php';
 
 $tables = ["pedido_contrato", "pedido_empenho", "pedido_fonte", "pedido_grupo", "pedido_id_fonte", "pedido_log_status", "pedido_plano"];
 
-Query::getInstance()->exe("SET foreign_key_checks = 0;");
+Query::getInstance()->exe("SET FOREIGN_KEY_CHECKS = 0;");
 $query = Query::getInstance()->exe("SELECT id FROM pedido");
 
 $i = 1;
@@ -12,12 +12,13 @@ $i = 1;
 while ($obj = $query->fetch_object()) {
     foreach ($tables as $table) {
         Query::getInstance()->exe("UPDATE " . $table . " SET id_pedido = " . $i . " WHERE id_pedido = " . $obj->id);
+        Query::getInstance()->exe("UPDATE pedido SET id= " . $i . " WHERE id= " . $obj->id);
         $i++;
     }
 }
 
 Query::getInstance()->exe("ALTER TABLE pedido AUTO_INCREMENT = " . $i);
 
-Query::getInstance()->exe("SET foreign_key_checks = 1;");
+Query::getInstance()->exe("SET FOREIGN_KEY_CHECKS = 1;");
 
 echo "Terminou!";
